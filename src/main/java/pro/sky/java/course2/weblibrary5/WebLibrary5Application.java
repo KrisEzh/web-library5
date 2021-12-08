7e5cab4b5e43328a17de5aee6abbc7024ca7793b
dns <pochti_zvezda@mail.ru>
2021-12-08 13:17:32 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/weblibrary5/WebLibrary5Application.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.weblibrary5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebLibrary5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebLibrary5Application.class, args);
+    }
+
+}
