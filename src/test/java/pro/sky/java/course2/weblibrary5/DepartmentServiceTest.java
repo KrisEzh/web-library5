0a698a9c8f71260facefc8011400095ac6cb1aa8
dns <pochti_zvezda@mail.ru>
2022-01-21 10:38:40 +0300

    dz13 все о том же мокито(

--- src/test/java/pro/sky/java/course2/weblibrary5/DepartmentServiceTest.java
+++ src/test/java/pro/sky/java/course2/weblibrary5/DepartmentServiceTest.java
@@ -4,70 +4,73 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
-import static pro.sky.java.course2.weblibrary5.EmployeeServiceConstants.*;
 
 
 @ExtendWith(MockitoExtension.class)
 public class DepartmentServiceTest {
-     private final EmployeeServiceImpl employeeServiceMock = mock(EmployeeServiceImpl.class);
+    // private final EmployeeServiceImpl employeeServiceMock = mock(EmployeeServiceImpl.class);
 
+    @Mock
+    private EmployeeServiceImpl employeeService;
 
     @BeforeEach
     public void initOut() {
-        out = new DepartmentServiceImpl(employeeServiceMock);
+        out = new DepartmentServiceImpl(employeeService);
     }
 
     @InjectMocks
     private DepartmentServiceImpl out;
 
-    private Employee employee = new Employee("Иван", "Иванов", 1, 10);
-    private Employee employee1 = new Employee("Петр", "Петров", 2, 20);
-    private Employee employee2 = new Employee("Сидр", "Сидоров", 3, 30);
+    @BeforeEach
+    public void setUp() {
+        Employee employee = new Employee("Иван", "Иванов", 1, 10);
+        Employee employee1 = new Employee("Петр", "Петров", 2, 20);
+        Employee employee2 = new Employee("Сидр", "Сидоров", 3, 30);
+
+        Collection<Employee> actual = new ArrayList<>();
+        when(employeeService.getEmployees()).thenReturn(actual);
+        actual.add(employee);
+        actual.add(employee1);
+        actual.add(employee2);
+
+    }
 
     public int MIN_SALARY = 10;
     public int MAX_SALARY = 30;
-    public int CORRECT_DEPARTMENT = 1;
 
-    @BeforeEach
-    public void setUp(){
-        Employee employee = new Employee(FIRST_NAME, LAST_NAME, DEPARTMENT_NUM, SALARY);
-        Employee employee1 = new Employee(FIRST_NAME1, LAST_NAME1, DEPARTMENT_NUM1, SALARY1);
-        Employee employee2 = new Employee(FIRST_NAME2, LAST_NAME2, DEPARTMENT_NUM2, SALARY2);
-
-
-        employeeServiceMock.add(employee);
-        employeeServiceMock.add(employee1);
-         employeeServiceMock.add(employee2);
-
-    }
 
     @Test
     public void getMaxSalaryTest() {
-        when(out.getMaxSalary(CORRECT_DEPARTMENT)).thenReturn();
-        assertEquals(MAX_SALARY, out.getMaxSalary(CORRECT_DEPARTMENT).getSalary());
+        assertEquals(MAX_SALARY, out.getMaxSalary(3).getSalary());
 
     }
+
     @Test
     public void getMinSalary() {
-        assertEquals(MIN_SALARY, out.getMinSalary(CORRECT_DEPARTMENT).getSalary());
-    }
-    @Test
-    public void getEmployeesForTest(){
-        Employee employee = new Employee("Иван", "Иванов", 1, 10);
-        Collection<Employee> expected = out.getEmployeesFor(1);
-        Collection<Employee> actual = new ArrayList<>();
-        when(out.getEmployeesFor(1)).thenReturn(actual);
-        employeeServiceMock.add(employee);
-        assertEquals(actual,expected);
+        assertEquals(MIN_SALARY, out.getMinSalary(1).getSalary());
     }
 
     @Test
-    public void employeeNotFoundExceptionTest(){
-        when(employeeServiceMock.getEmployees()).thenThrow(EmployeeNotFoundException.class);
+    public void getEmployeesForTest() {
+        Employee employee = new Employee("Иван", "Иванов", 1, 10);
+        Collection<Employee> actual = new ArrayList<>();
+        when(out.getEmployeesFor(1)).thenReturn(actual);
+        actual.add(employee);
+        assertEquals(employeeService.getEmployees(), out.getEmployeesFor(1));
+    }
+
+    @Test
+    public void employeeNotFoundExceptionTest() {
+        if (employeeService.getEmployees() == null) {
+            when(employeeService.getEmployees()).thenThrow(EmployeeNotFoundException.class);
+
+        }
     }
 }
